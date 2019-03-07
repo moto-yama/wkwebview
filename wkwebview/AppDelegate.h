331fb1cc20adff492b044ef954f0090ece92ea7f
asto_admin1 <asto_admin1@asto-system.com>
2019-03-07 09:50:16 +0900

    init

---
+++ wkwebview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wkwebview
+//
+//  Created by Yuichiro Motoyama on 2019/03/07.
+//  Copyright © 2019 moto-yama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wkwebview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wkwebview
+//
+//  Created by Yuichiro Motoyama on 2019/03/07.
+//  Copyright © 2019 moto-yama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
